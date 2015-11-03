f6c048b3a860e27a1c8170f2ad71c6b862720158
Marcin Kik <mki1967@gmail.com>
2015-11-03 09:30:08 +0100

    testing buttons layout in the viewer

--- html_viewer/mki3d_view.js
+++ html_viewer/mki3d_view.js
@@ -86,7 +86,7 @@ window.onload= function(){
     // mki3d.setProjectionMatrix();
     // mki3d.setModelViewMatrix();
     // mki3d.action.init(); // mki3d.action requires initialization
-    mki3d.html.divUpperMessage.innerHTML += "  (Press 'H' for help.)";
+    // mki3d.html.divUpperMessage.innerHTML += "  (Press 'H' for help.)";
     // mki3d.redraw();
     // window.onkeydown=mki3d.callback.canvasOnKeyDown;
     mki3d.callback.onWindowResize();
