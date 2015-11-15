3cc1bfd0211abbe16e12f329ca39d7013ee77d21
Marcin Kik <mki1967@gmail.com>
2015-11-15 09:47:48 +0100

    some updates of the export template

--- mki3d_www/js/mki3d_template.js
+++ mki3d_www/js/mki3d_template.js
@@ -4,33 +4,36 @@ mki3d.template={};
 mki3d.template.exportedHtml="\
 <!DOCTYPE html > \
 <html id=\"htmlId\" style=\"overflow: hidden;\"> \
-  <body> \
-    <div id=\"divCanvas\" style=\"position: relative;\"> \
-      <canvas id=\"canvasId\" style=\"border:0px; margin: 0px; padding: 0px;\" > \
-	Your browser does not support the canvas element. \
-      </canvas> \
-      <div id=\"divUpperMessage\" style=\"position: absolute; top: 0px; left: 0px; color: black; background-color: white;\"> \
-	DIRECTIONS:\
-        <button type=\"button\" id=\"leftButton\"> LEFT </button> \
-	<button type=\"button\" id=\"upButton\"> UP </button> \
-	<button type=\"button\" id=\"backButton\"> BACK </button> \
-	<button type=\"button\" id=\"forwardButton\"> FORWARD </button> \
-	<button type=\"button\" id=\"downButton\"> DOWN </button> \
-	<button type=\"button\" id=\"rightButton\"> RIGHT </button> \
-	ACTIONS: \
- 	<button type=\"button\" id=\"rotateButton\"> ROTATE </button> \
-	<button type=\"button\" id=\"moveButton\"> MOVE </button> \
-	<button type=\"button\" id=\"scaleUpButton\"> SCALE UP </button> \
-	<button type=\"button\" id=\"scaleDownButton\"> SCALE DOWN </button> \
-	<button type=\"button\" id=\"alignButton\"> ALIGN </button> \
-	<button type=\"button\" id=\"resetButton\"> RESET </button> \
-	<a href=\"https://github.com/mki1967/mki3d/tree/master/html_viewer\"> INFO </a> \
-      </div> \
-    </div> \
-    <script src=\"mki3d_view.js\"> \
-    </script> \
-    <script> \
-      mki3d.exported= {/* replace */} \
-    </script> \
-  </body> \
-</html>";
+<body> \
+<div id=\"divCanvas\" style=\"position: relative;\"> \
+<canvas id=\"canvasId\" style=\"border:0px; margin: 0px; padding: 0px;\" > \
+Your browser does not support the canvas element. \
+</canvas> \
+<div id=\"divUpperMessage\" style=\"position: absolute; top: 0px; left: 0px; color: black; background-color: white;\"> \
+DIRECTIONS:\
+<button type=\"button\" id=\"leftButton\"> LEFT </button> \
+<button type=\"button\" id=\"upButton\"> UP </button> \
+<button type=\"button\" id=\"backButton\"> BACK </button> \
+<button type=\"button\" id=\"forwardButton\"> FORWARD </button> \
+<button type=\"button\" id=\"downButton\"> DOWN </button> \
+<button type=\"button\" id=\"rightButton\"> RIGHT </button> \
+ACTIONS: \
+<button type=\"button\" id=\"rotateButton\"> ROTATE </button> \
+<button type=\"button\" id=\"moveButton\"> MOVE </button> \
+<button type=\"button\" id=\"scaleUpButton\"> SCALE UP </button> \
+<button type=\"button\" id=\"scaleDownButton\"> SCALE DOWN </button> \
+<button type=\"button\" id=\"alignButton\"> ALIGN </button> \
+<button type=\"button\" id=\"resetButton\"> RESET </button> \
+<a href=\"https://github.com/mki1967/mki3d/tree/master/html_viewer\"> INFO </a> \
+</div> \
+</div> \
+<script src=\"mki3d_view.js\"> \
+</script> \
+<script> \
+\n\
+mki3d.exported= {/* replace */} \
+\n\
+</script> \
+</body> \
+</html> \
+\n";
