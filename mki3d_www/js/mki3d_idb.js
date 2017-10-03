b59dfa2660126a52ec820d3254c43580a8e92b79
Marcin Kik <mki1967@gmail.com>
2017-10-03 09:15:34 +0200

    indexedDB ...

--- mki3d_www/js/mki3d_idb.js
+++ mki3d_www/js/mki3d_idb.js
@@ -7,14 +7,31 @@ if (!window.indexedDB) {
 }
 
 
-mkid.idb.onupgradeneeded = function( event ) {
+mki3d.idb.onupgradeneeded = function( event ) {
     // PODSTAWIAĆ JAKO CALLBACK DO REQUEST ...
     
     // PRZEROBIĆ:
     var db = event.target.result;
 
     // Create an objectStore for this database
-    var objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+    var objectStore = db.createObjectStore("files", {keyPath: 'id', autoIncrement:true});
 
 }
 
+mki3d.idb.dbName = "mki3d" // name of the database
+
+
+mki3d.idb.openDB = function( ){
+    var request = indexedDB.open(mki3d.idb.dbName);
+    
+    request.onupgradeneeded = mki3d.idb.onupgradeneeded;
+    
+    request.onerror = function( event) {
+	console.log( event );
+    };
+    
+    request.onsuccess = function( event ) {
+	mki3d.idb.db = event.target.result;
+    };
+
+}
