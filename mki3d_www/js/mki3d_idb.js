777955624691146a874ce1acee118c330b72c111
Marcin Kik <mki1967@gmail.com>
2017-10-10 09:17:24 +0200

    working on idb ...

--- mki3d_www/js/mki3d_idb.js
+++ mki3d_www/js/mki3d_idb.js
@@ -12,11 +12,64 @@ mki3d.idb.onupgradeneeded = function( event ) {
     
     // PRZEROBIĆ:
     var db = event.target.result;
-
-    // Create an objectStore for this database
-    var objectStore = db.createObjectStore("files", {keyPath: 'id', autoIncrement:true});
+    
+    // Create an objectStore and date as its index 
+    if(!thisDB.objectStoreNames.contains("files")) {
+	var os = thisDB.createObjectStore("files", {keyPath: 'key', autoIncrement:true});
+	os.createIndex("date", "", {unique:false});
+    }
 
 }
+
+mki3d.idb.filesFound= [];
+
+mki3d.idb.findFiles= function() {
+    /* 
+       REMADE FROM EXAMPLE:
+       https://static.raymondcamden.com/demos/2013/jun/6/test1.html
+    */
+    var fromDate = document.querySelector("#inputIDBFromDate").value;
+    var toDate = document.querySelector("#inputIDBToDate").value;
+    var substring = document.querySelector("#inputIDBNameSubString").value;
+    var range=null;
+
+    if(fromDate == "" && toDate == "") return;
+
+    // console.log('doSearch',fromDate,toDate);
+    var transaction = db.transaction(["files"],"readonly");
+    var store = transaction.objectStore("files");
+    var index = store.index("date");
+
+    if(fromDate != "") fromDate = new Date(fromDate);
+    if(toDate != "") toDate = new Date(toDate);
+    
+    if(fromDate != "" && toDate != "") {
+	range = IDBKeyRange.bound(fromDate, toDate);
+    } else if(fromDate == "") {
+	range = IDBKeyRange.upperBound(toDate);
+    } else if(toDate == "") {
+	range = IDBKeyRange.lowerBound(fromDate);
+    }
+
+    mki3d.idb.filesFound= [];
+    console.dir(range); /// test
+    
+    index.openCursor(range).onsuccess = function(e) {
+	var cursor = e.target.result;
+        console.log('got something');
+	var name =  cursor.value['name']
+	if(cursor) && ( name.includes(substring) ) {
+	    item= {} ;
+	    item.key = cursor.key;
+	    item.date = cursor.value['date'];
+	    item.name = name;
+	    mki3d.idb.filesFound.push(item);
+	    cursor.continue();
+	}
+    }
+
+}
+
 
 mki3d.idb.dbName = "mki3d" // name of the database
 
@@ -35,3 +88,12 @@ mki3d.idb.openDB = function( ){
     };
 
 }
+
+mki3d.idb.prepareItem= function(){
+    var item = {}
+    item.name = mki3d.file.suggestedName.repeat(1); // copy
+    item.date = new Date();
+    item.dataString = JSON.stringify(mki3d.data);
+    return item;
+}
+
